c252dac4b07564079c98e9a43430efdd2c2d37ab
Guido <guidotoritto@outlook.com>
2023-10-24 20:02:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Proximamente</h1>
+        <h5>Casedif | La nueva forma de construir</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
