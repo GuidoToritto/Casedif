b1ec706da33a1a0b2dae826a22e53f05ae87097b
Guido <guidotoritto@outlook.com>
2023-11-07 15:11:01 -0300

    fix

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,9 @@ import Whatsapp from "../images/whatsapp-svgrepo-com (2).png"
 export const Work = () => {
   return (
     <div className="work">       
+      <a href="https://wa.me/5491176180939?">
       <img src={Whatsapp} className="logo-principal" alt="" />
+      </a>
     </div>
   );
 };
